292519a906ecc62c443076fa8a3a2a11cb7a0f3b
Nada Mohamed <nadamohamed1620019@gmail.com>
2026-02-03 10:32:34 +0200

    refactor: improve user registration logic by destructuring request body and refining user object handling.

--- Controller/userEndpoints.js
+++ Controller/userEndpoints.js
@@ -11,30 +11,38 @@ const getAllUsers = asyncWrapper(async (req, res, next) => {
     const limit = parseInt(query.limit) || 0;
     const page = parseInt(query.page) || 1;
     const skip = (page - 1) * limit;
-  
-    const users = await User.find({}, { __v: 0 , password: 0}).limit(limit).skip(skip);
-  
+
+    const users = await User.find({}, { __v: 0, password: 0 }).limit(limit).skip(skip);
+
     res.status(200).json({ status: STATUS.SUCCESS, data: users });
 });
 
-const registerUser = asyncWrapper(async(req, res, next) => {
-    const user = new User(req.body);
-    const oldUser = await User.findOne({ email: req.body.email });
+const registerUser = asyncWrapper(async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+    const oldUser = await User.findOne({ email: email });
     if (oldUser) {
-        return next(new AppError("User already exists with this email", 400));
+        const error = new AppError("User already exists", 400);
+        return next(error);
     }
-    const salt = await becrypt.genSalt(10);
-    user.password = await becrypt.hash(user.password, salt);
-    await user.save();
-    delete user.password;
-    res.status(201).json({ status: STATUS.SUCCESS, data: user });
+    const hashedPassword = await becrypt.hash(password, 10);
+    const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+    });
+    await newUser.save();
+    const userResponse = newUser.toObject();
+    delete userResponse.password;
+
+    res.status(201).json({ status: STATUS.SUCCESS, data: { user: userResponse } });
 })
 
 const LoginUser = asyncWrapper(async (req, res, next) => {
-        
+
 })
 
-module.exports ={
+module.exports = {
     getAllUsers,
     registerUser,
     LoginUser
